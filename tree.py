a4d429d81ca0aa0de8c343470d3d3678fc131cba
ludwing979 <magoludwing@gmail.com>
2021-07-08 10:59:28 -0500

    'clase'

--- tree.py
+++ tree.py
@@ -17,6 +17,11 @@ class Node:
 
         print("Value is not in tree")
 
+    def height(self, h=0):
+        leftHeight = self.left.height(h+1) if self.left else h
+        rightHeight = self.right.height(h+1) if self.right else h
+        return max(leftHeight, rightHeight)
+
 
 class Tree:
     def __init__(self, root, name=''):
@@ -25,6 +30,9 @@ class Tree:
 
     def search(self, target):
         return self.root.search(target)
+
+    def height(self):
+        return self.root.height()
 
 
 tree = Tree(Node(50), 'A Very Tall Tree')
@@ -37,3 +45,9 @@ tree.root.left.right.right = Node(42)
 tree.root.left.left.left = Node(5)
 tree.root.left.left.right = Node(13)
 tree.root.left.left.left.left = Node(2)
+
+print(tree.height())
+
+tree = Tree(Node(50), 'A Very Tall Tree')
+print(tree.height())
+
